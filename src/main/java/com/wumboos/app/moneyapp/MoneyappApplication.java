c840a7ca19c83482768dedff78e556214f288b5f
anton.prasetyo <anton.prasetyo@primecash.co.id>
2023-12-20 17:28:37 +0700

    add h2 r2dbc

--- src/main/java/com/wumboos/app/moneyapp/MoneyappApplication.java
+++ src/main/java/com/wumboos/app/moneyapp/MoneyappApplication.java
@@ -1,10 +1,17 @@
 package com.wumboos.app.moneyapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 import org.springframework.web.reactive.config.EnableWebFlux;
+
+import io.r2dbc.spi.ConnectionFactory;
 
 @EnableWebFlux
 @SpringBootApplication
@@ -14,5 +21,14 @@ public class MoneyappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoneyappApplication.class, args);
 	}
+	
+    @Bean
+    ConnectionFactoryInitializer initializer(@Qualifier("connectionFactory") ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        ResourceDatabasePopulator resource = new ResourceDatabasePopulator(new ClassPathResource("init.sql"));
+        initializer.setDatabasePopulator(resource);
+        return initializer;
+    }
 
 }
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,2 +1,1 @@
-CREATE DATABASE moneyapp;
 CREATE TABLE IF NOT EXISTS tutorial (id INT NOT NULL, title VARCHAR(255), description VARCHAR(255), published BOOLEAN, PRIMARY KEY (id));
