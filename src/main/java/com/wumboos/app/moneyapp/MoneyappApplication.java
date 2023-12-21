c0a7b116db25b1fe5233f2a368e7227b11e897d5
wumboos <wumboos@gmail.com>
2023-12-21 23:48:56 +0700

    create new gradle task dev & add flyway migration db

--- src/main/java/com/wumboos/app/moneyapp/MoneyappApplication.java
+++ src/main/java/com/wumboos/app/moneyapp/MoneyappApplication.java
@@ -1,17 +1,12 @@
 package com.wumboos.app.moneyapp;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 import org.springframework.web.reactive.config.EnableWebFlux;
-
-import io.r2dbc.spi.ConnectionFactory;
 
 @EnableWebFlux
 @SpringBootApplication
@@ -21,14 +16,14 @@ public class MoneyappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoneyappApplication.class, args);
 	}
-	
-    @Bean
-    ConnectionFactoryInitializer initializer(@Qualifier("connectionFactory") ConnectionFactory connectionFactory) {
-        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
-        initializer.setConnectionFactory(connectionFactory);
-        ResourceDatabasePopulator resource = new ResourceDatabasePopulator(new ClassPathResource("init.sql"));
-        initializer.setDatabasePopulator(resource);
-        return initializer;
-    }
+
+//    @Bean
+//    ConnectionFactoryInitializer initializer(@Qualifier("connectionFactory") ConnectionFactory connectionFactory) {
+//        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+//        initializer.setConnectionFactory(connectionFactory);
+//        ResourceDatabasePopulator resource = new ResourceDatabasePopulator(new ClassPathResource("init.sql"));
+//        initializer.setDatabasePopulator(resource);
+//        return initializer;
+//    }
 
 }
---
+++ src/main/resources/db/migration/V1__create_table_tutorial.sql
@@ -0,0 +1,1 @@
+CREATE TABLE tutorial (id INT NOT NULL, title VARCHAR(255), description VARCHAR(255), published BOOLEAN, PRIMARY KEY (id));
