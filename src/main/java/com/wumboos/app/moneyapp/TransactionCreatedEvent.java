514060ede4fb8295a4113e62167fc0cdd3397662
wumboos <wumboos@gmail.com>
2023-12-24 23:49:50 +0700

    test publish event on reactive transaction

---
+++ src/main/java/com/wumboos/app/moneyapp/TransactionCreatedEvent.java
@@ -0,0 +1,9 @@
+package com.wumboos.app.moneyapp;
+
+import java.util.UUID;
+
+import org.jmolecules.event.types.DomainEvent;
+
+public record TransactionCreatedEvent(UUID id){
+
+}
--- src/main/java/com/wumboos/app/moneyapp/tutorial/service/TutorialService.java
+++ src/main/java/com/wumboos/app/moneyapp/tutorial/service/TutorialService.java
@@ -1,11 +1,20 @@
 package com.wumboos.app.moneyapp.tutorial.service;
 
 import java.util.Optional;
+import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.transaction.reactive.TransactionalEventPublisher;
 
+import com.wumboos.app.moneyapp.TransactionCreatedEvent;
 import com.wumboos.app.moneyapp.tutorial.model.Tutorial;
 import com.wumboos.app.moneyapp.tutorial.repository.TutorialRepository;
 
@@ -14,50 +23,61 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class TutorialService {
+	Logger log = LoggerFactory.getLogger(TutorialService.class);
 
-  @Autowired
-  TutorialRepository tutorialRepository;
-  
-  @Autowired
-  private ApplicationEventPublisher events;
+	@Autowired
+	TutorialRepository tutorialRepository;
 
-  public Flux<Tutorial> findAll() {
-    return tutorialRepository.findAll();
-  }
+	@Autowired
+	private ApplicationEventPublisher eventPublisher;
 
-  public Flux<Tutorial> findByTitleContaining(String title) {
-    return tutorialRepository.findByTitleContaining(title);
-  }
+	@Transactional
+	public Flux<Tutorial> findAll() {
+		return new TransactionalEventPublisher(this.eventPublisher)
+				.publishEvent(new TransactionCreatedEvent(UUID.randomUUID())).log()
+	    .as(Flux::from).log().flatMap(s -> tutorialRepository.findAll());
+	}
+	
+	@Async
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	@TransactionalEventListener
+	Mono<Void> on(TransactionCreatedEvent event) {
+		return Mono.just(event).log().then();
+	}
 
-  public Mono<Tutorial> findById(int id) {
-    return tutorialRepository.findById(id);
-  }
+	public Flux<Tutorial> findByTitleContaining(String title) {
+		return tutorialRepository.findByTitleContaining(title);
+	}
 
-  public Mono<Tutorial> save(Tutorial tutorial) {
-    return tutorialRepository.save(tutorial);
-  }
+	public Mono<Tutorial> findById(int id) {
+		return tutorialRepository.findById(id);
+	}
 
-  public Mono<Tutorial> update(int id, Tutorial tutorial) {
-    return tutorialRepository.findById(id).map(Optional::of).defaultIfEmpty(Optional.empty())
-        .flatMap(optionalTutorial -> {
-          if (optionalTutorial.isPresent()) {
-            tutorial.setId(id);
-            return tutorialRepository.save(tutorial);
-          }
+	public Mono<Tutorial> save(Tutorial tutorial) {
+		return tutorialRepository.save(tutorial);
+	}
 
-          return Mono.empty();
-        });
-  }
+	public Mono<Tutorial> update(int id, Tutorial tutorial) {
+		return tutorialRepository.findById(id).map(Optional::of).defaultIfEmpty(Optional.empty())
+				.flatMap(optionalTutorial -> {
+					if (optionalTutorial.isPresent()) {
+						tutorial.setId(id);
+						return tutorialRepository.save(tutorial);
+					}
 
-  public Mono<Void> deleteById(int id) {
-    return tutorialRepository.deleteById(id);
-  }
+					return Mono.empty();
+				});
+	}
 
-  public Mono<Void> deleteAll() {
-    return tutorialRepository.deleteAll();
-  }
+	public Mono<Void> deleteById(int id) {
+		return tutorialRepository.deleteById(id);
+	}
 
-  public Flux<Tutorial> findByPublished(boolean isPublished) {
-    return tutorialRepository.findByPublished(isPublished);
-  }
+	public Mono<Void> deleteAll() {
+		return tutorialRepository.deleteAll();
+	}
+
+	public Flux<Tutorial> findByPublished(boolean isPublished) {
+		return tutorialRepository.findByPublished(isPublished);
+	}
 }
